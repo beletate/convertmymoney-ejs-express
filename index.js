6f7e769ebc2684f8d7a86a05dd181521a7e9b528
Vinicius Beletate <vinicius_beletate@hotmail.com>
2022-12-15 18:31:24 -0300

    test(jest): created test to help code

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const app = express();
 const path = require('path');
 
 const convert = require('./lib/convert');
+const apiBCB = require('./lib/apiusdbrl');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get('/', async (req, res) => {
+    const cotacao = await apiBCB.getCotacao();
+    res.render('home', { cotacao: parseFloat(cotacao) });
 })
 
 app.get('/cotacao', (req, res) => {
     const { cotacao, quantidade } = req.query;
-    if(cotacao && quantidade){
+    if (cotacao && quantidade) {
         const conversao = convert.convert(cotacao, quantidade);
         res.render('cotacao', {
             error: false,
